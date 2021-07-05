e5bb3ba25580d4936eb60aa86d23d93976321738
mudassir097 <mudassir.nazar097@gmail.com>
2021-07-06 00:11:31 +0530

    state wise html injection

--- ss.js
+++ ss.js
@@ -69,17 +69,139 @@ fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api_india", {
              
               
   
+              let state = [ "Andhra Pradesh",
+              "Arunachal Pradesh",
+              "Assam",
+              "Bihar",
+              "Chhattisgarh",
+              "Goa",
+              "Gujarat",
+              "Haryana",
+              "Himachal Pradesh",
+              "Jammu and Kashmir",
+              "Jharkhand",
+              "Karnataka",
+              "Kerala",
+              "Madhya Pradesh",
+              "Maharashtra",
+              "Manipur",
+              "Meghalaya",
+              "Mizoram",
+              "Nagaland",
+              "Odisha",
+              "Punjab",
+              "Rajasthan",
+              "Sikkim",
+              "Tamil Nadu",
+              "Telangana",
+              "Tripura",
+              "Uttarakhand",
+              "Uttar Pradesh",
+              "West Bengal",
+              "Andaman and Nicobar Islands",
+              "Chandigarh",
+              "Dadra and Nagar Haveli",
+              "Daman and Diu",
+              "Delhi",
+              "Lakshadweep",
+              "Puducherry"];
+
+              
+
+ 
+              for (var i = 0; i < state.length; i++) {
+
+                let state_value = data.state_wise.state[i];
+
+                console.log(state_value);
 
 
-  let state_value = data.state_wise;
 
-
-
+              }  
  
 
 
 
-  document.querySelector('#state_live').innerHTML = html;
+  document.querySelector('#state_live').innerHTML = `
+  
+              <div class="row">
+                <div class="col-lg-12 mbr-col-md-10 text-center">
+                  <div class="card wrap">
+                    <div class="card-header">
+                    
+                    <h1><span class="badge badge-secondary">state code</span>state name</h1>
+                    
+                    </div>
+                    <div class=" card-body">
+                    
+                    
+
+
+
+
+                          <div class="col-lg-2 col-md-4 body-align">
+                      
+                          <div class=" ">
+                            <i class="fa fa-3x fa-stethoscope" style=" color: rgba(17, 100, 224, 0.685);" aria-hidden="true"></i>
+                            <div class="count" style="font-size:100%;">	<p>${active_value}</p></div>
+                            <div class="title" style="font-family:verdana;">Active</div>
+                          </div>
+
+                          </div>
+                          <div class="col-lg-2 col-md-4 body-align">                  
+                            <div class="">
+                              <i class="fa fa-3x fa-heart" style=" color: rgb(231, 37, 37);" aria-hidden="true" ></i>
+                              <div class="count" style="font-size:100%;"><p>${recovered_value}</p> </div>
+                              <div class="title" style="font-family:verdana;">Recovered</div>
+                            </div>
+                          </div>
+                          <div class=col-lg-2 col-md-4 body-align">                  
+                            <div class=" ">
+                              <i class="fa fa-3x fa-industry" style=" color: rgba(32, 53, 85, 0.699);" aria-hidden="true"></i>
+                              <div class="count" style="font-size:100%; "><p>${total_case}</p></div>
+                              <div class="title" >Total</div>
+                            </div>
+                          </div>
+                          <div class="col-lg-2 col-md-4 body-align">                  
+                            <div class="">
+                              <i class="fa fa-3x fa-certificate" style=" color: rgba(224, 208, 26, 0.548);" aria-hidden="true"></i>
+                              <div class="count" style="font-size:100%;"><p>${death_case}</p></div>
+                              <div class="title">Death</div>
+                            </div>
+                          </div>
+                          <div class="col-lg-2 col-md-4 body-align">                  
+                            <div class=" ">
+                              <i class="fa fa-3x fa-calendar" style=" color: rgba(40, 238, 195, 0.884);" aria-hidden="true"></i>
+                              <div class="count" style="font-size:100%;"><p>${last_update}</p></div>
+                              <div class="title">time</div>
+                            </div>
+                          </div>
+
+
+                    </div>
+                 
+                  </div>
+              
+                </div>
+              <div class="row mbr-justify-content-center">
+                <div class="col-lg-12 mbr-col-md-10 text-center">
+                  <div class="card wrap">
+                    <div class="card-header"></div>
+                    <div class="card-body">Content</div>
+                    <div class="card-footer">Footer</div>
+                  </div>
+                </div>
+              </div>
+              <div class="row mbr-justify-content-center">
+                <div class="col-lg-12 mbr-col-md-10 text-center">
+                  <div class="card wrap">
+                    <div class="card-header"></div>
+                    <div class="card-body">Content</div>
+                    <div class="card-footer">Footer</div>
+                  </div>
+                </div>
+              </div>
+  `;
 
 
  });
@@ -88,8 +210,8 @@ fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api_india", {
 
 
 
-
-
+/* 
+for (var i = 0; i < grpd[date_key].length; i++) */
 
 
 
